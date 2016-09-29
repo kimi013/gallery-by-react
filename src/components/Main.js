11ddce9922ea9bc6cfd33bc5f939f7a9319d269c
kimi013 <yliu234@126.com>
2016-09-30 01:00:54 +0800

    add figures and add styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -20,13 +20,38 @@ imageDatas = (function getImageURL(imageDatasArr) {
 })(imageDatas);
 
 
+class ImgFigure extends React.Component {
+  render() {
+    return (
+      <figure className = "img-figure">
+        <img src = {this.props.data.imageURL}
+             alt = {this.props.data.title}/>
+        <figcaption>
+          <h2 className = "img-title">{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    );
+  }
+}
+
+
 class AppComponent extends React.Component {
   render() {
+
+    let controllerUnits = [],
+        imgFigures = [];
+
+    imageDatas.forEach(function (value, index) {
+      imgFigures.push(<ImgFigure data = {value} key = {index} />);
+    });
+
     return (
       <section className = "stage">
         <section className = "img-sec">
+          {imgFigures}
         </section>
         <nav className = "controller-nav">
+          {controllerUnits}
         </nav>
       </section>
     );
